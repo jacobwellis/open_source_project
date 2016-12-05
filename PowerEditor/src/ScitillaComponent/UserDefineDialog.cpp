0809a64c57e703206ba7d7a93dc822b0fdb39db9
Fran Gonzalez <gonzalezfrancis@hotmail.com>
2016-12-05 11:23:12 +0000

    Implementing the wordcount algorithm

--- PowerEditor/src/ScitillaComponent/UserDefineDialog.cpp
+++ PowerEditor/src/ScitillaComponent/UserDefineDialog.cpp
@@ -24,7 +24,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program; if not, write to the Free Software
 // Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
-//Adding the word count
+//Adding the word count algorithm
 
 #include "localization.h"
 #include "UserDefineDialog.h"
